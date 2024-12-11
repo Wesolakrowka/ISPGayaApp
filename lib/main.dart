93fbe24036bd9041f25d0f7b57cbd750aec62f98
Martyna Dawidowicz <ul0263726@edu.uni.lodz.pl>
2024-12-11 14:46:37 +0100

    Początkowy commit projektu

---
+++ lib/main.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+
+
+
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key, required String title});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late final TextEditingController _email;
+  late final TextEditingController _password;
+  @override
+  void initState() {
+    _email= TextEditingController();
+    _password= TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _email.dispose();
+    _password.dispose();
+    super.dispose();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar: AppBar(title: const Text('Register'),),
+    body: Column(
+      children: [
+        TextField(controller: _email,
+        decoration: const InputDecoration(hintText:"Username" ),),
+        TextField(controller: _password,
+        decoration: const InputDecoration(hintText:"Password" ),),
+        TextButton(onPressed: () async {
+          
+        },
+         child: const Text('email'),
+         ),
+      ],
+    ),
+    );
+  }
+}
