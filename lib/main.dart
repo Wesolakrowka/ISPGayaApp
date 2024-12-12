8d095f12339b87595195bb4d061a21fd80a763f6
Martyna Dawidowicz <ul0263726@edu.uni.lodz.pl>
2024-12-12 16:09:16 +0100

    First layer of login

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,20 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-
+import 'firebase_options.dart';
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
@@ -23,12 +23,10 @@ class MyApp extends StatelessWidget {
   }
 }
 
-
-
-
-
 class HomePage extends StatefulWidget {
-  const HomePage({super.key, required String title});
+  final String title;
+  
+  const HomePage({super.key, required this.title});
 
   @override
   State<HomePage> createState() => _HomePageState();
@@ -37,10 +35,11 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   late final TextEditingController _email;
   late final TextEditingController _password;
+
   @override
   void initState() {
-    _email= TextEditingController();
-    _password= TextEditingController();
+    _email = TextEditingController();
+    _password = TextEditingController();
     super.initState();
   }
 
@@ -50,22 +49,57 @@ class _HomePageState extends State<HomePage> {
     _password.dispose();
     super.dispose();
   }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: const Text('Register'),),
-    body: Column(
-      children: [
-        TextField(controller: _email,
-        decoration: const InputDecoration(hintText:"Username" ),),
-        TextField(controller: _password,
-        decoration: const InputDecoration(hintText:"Password" ),),
-        TextButton(onPressed: () async {
-          
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Register'),
+      ),
+      body: FutureBuilder(
+        future:  Firebase.initializeApp(
+                  options: DefaultFirebaseOptions.currentPlatform,
+        ),
+        builder: (context, snapshot){
+          switch (snapshot.connectionState){
+            case ConnectionState.done:
+                return Column(
+                children: [
+                  TextField(
+                    controller: _email,
+                    autocorrect: false,
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: const InputDecoration(hintText: "Username"),
+                  ),
+                  TextField(
+                    controller: _password,
+                    obscureText: true,
+                    enableSuggestions: false,
+                    autocorrect: false,
+                    decoration: const InputDecoration(hintText: "Password"),
+                  ),
+                  TextButton(
+                    onPressed: () async {
+                    
+                    
+                      final email = _email.text;
+                      final password = _password.text;
+                      final userCredential = await FirebaseAuth.instance.createUserWithEmailAndPassword(
+                        email: email, 
+                        password: password
+                      );
+                      print(userCredential);
+                    },
+                    child: const Text('Register'),
+                  )
+                ],
+              );
+            default:
+            return const Text("loading...");
+          }  
         },
-         child: const Text('email'),
-         ),
-      ],
-    ),
+        
+      ),
     );
   }
 }
